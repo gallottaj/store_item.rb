99c7b1747ef77ad4a91fb9f5af1caaa8abac0866
Joe Gallotta <joeygallotta@gmail.com>
2018-10-08 21:18:27 -0500

    print word + definition

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,9 +1,16 @@
 #hardcode using "dolphin"
 
+#input - an entered word
+#output - a printed definition
 
 require 'unirest'
 
-response = Unirest.get("https://api.wordnik.com/v4/word.json/dolphin?useCanonical=false&includeSuggestions=true&api_key=8dba4744d147470de953d228e1788d494480af9c8a004769a")
+response = Unirest.get("https://api.wordnik.com/v4/word.json/dolphin/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=8dba4744d147470de953d228e1788d494480af9c8a004769a")
 
-puts JSON.pretty_generate(response.body)
+word = response.body[0]['word']
+definition = response.body[0]['text']
+
+p "The definition of #{word} is: #{definition}"
+
+
 
