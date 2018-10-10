66d26363cd0c61906bb2bf725f4950d39d68b0d4
Joe Gallotta <joeygallotta@gmail.com>
2018-10-10 09:24:01 -0500

    made time display more user friendly

--- tell_time-app/app/controllers/api/pages_controller.rb
+++ tell_time-app/app/controllers/api/pages_controller.rb
@@ -1,7 +1,8 @@
 class Api::PagesController < ApplicationController
 
   def time_action
-    @tell_time = Time.now
+    @tell_time = Time.now.strftime("%b %e, %l:%M %p")
     render "tell_time_view.json.jbuilder"
   end
 end
+
