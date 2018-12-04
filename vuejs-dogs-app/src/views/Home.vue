113b4b1a7450582559cf66c29bdab2149ed8728d
Joe Gallotta <joeygallotta@gmail.com>
2018-12-03 19:32:21 -0600

    init commit

---
+++ vuejs-dogs-app/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  <li v-for="dog in dogs">
+    {{ dog.meme_phrase }}
+  </li>
+</ul>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require('axios');
+
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      dogs:[]
+    };
+  },
+  created: function() {
+    axios.get('http://localhost:3000/api/dogs').then(function(response) {
+      console.log(response.data);
+      this.dogs = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
+</script>
